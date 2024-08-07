2ba156690796550e50b5408aab6e6d0a6cafb92f
alpertunga-bile <kingdom19online@hotmail.com>
2024-08-07 17:25:45 +0300

    new ctors are added

--- main.cpp
+++ main.cpp
@@ -16,23 +16,47 @@ PYBIND11_MODULE(simdstring, m) {
 
   auto str_class = py::class_<StrClass>(m, "SIMDString");
 
-  str_class.def(py::init<const char *>());
+  /*
+   * Starting from the 499th line of the SIMDString.h file
+   */
+
+  str_class.def(py::init<std::nullptr_t>());
+  str_class.def(py::init<>());
+  str_class.def(py::init<std::size_t, StrClass::value_type>(), "count"_a,
+                "c"_a);
+  str_class.def(py::init<const StrClass::value_type>(), "c"_a);
+  str_class.def(py::init<const StrClass &, std::size_t>(), "str"_a, "pos"_a);
+  str_class.def(py::init<const StrClass &, std::size_t, std::size_t>(), "str"_a,
+                "pos"_a, "count"_a);
+  str_class.def(py::init<StrClass::const_pointer>(), "s"_a);
+  str_class.def(py::init<StrClass::const_pointer, std::size_t>(), "s"_a,
+                "count"_a);
+  str_class.def(py::init<StrClass::const_pointer, std::size_t, std::size_t>(),
+                "s"_a, "pos"_a, "count"_a);
+  str_class.def(py::init<const std::string &, std::size_t, std::size_t>(),
+                "str"_a, "pos"_a, "count"_a);
+  str_class.def(py::init<std::initializer_list<StrClass::value_type>>(),
+                "ilist"_a);
   str_class.def(py::init<const std::string &>());
   str_class.def(py::init<std::string &&>());
+
+  // addition operations
 
   str_class.def(py::self + py::self);
   str_class.def(py::self += py::self);
 
   // check operations
 
-  str_class.def(py::self > py::self);
-  str_class.def(py::self < py::self);
-  str_class.def(py::self >= py::self);
-  str_class.def(py::self <= py::self);
-  str_class.def(py::self == py::self);
-  str_class.def(py::self == StrClass::const_pointer());
-  str_class.def(py::self != py::self);
-  str_class.def(py::self != StrClass::const_pointer());
+  str_class.def(py::self > py::self, "str"_a);
+  str_class.def(py::self < py::self, "str"_a);
+  str_class.def(py::self >= py::self, "str"_a);
+  str_class.def(py::self <= py::self, "str"_a);
+  str_class.def(py::self == py::self, "str"_a);
+  str_class.def(py::self == StrClass::const_pointer(), "s"_a);
+  str_class.def(py::self != py::self, "str"_a);
+  str_class.def(py::self != StrClass::const_pointer(), "s"_a);
+
+  // find functions
 
   str_class.def("find",
                 py::overload_cast<const StrClass &, std::size_t>(
@@ -52,6 +76,8 @@ PYBIND11_MODULE(simdstring, m) {
                     &StrClass::find, py::const_),
                 "s"_a, "pos"_a);
 
+  // variable functions
+
   str_class.def("length", &StrClass::length);
   str_class.def("data", py::overload_cast<>(&StrClass::data, py::const_));
   str_class.def("equals", &StrClass::equals, "str"_a);
@@ -62,22 +88,29 @@ PYBIND11_MODULE(simdstring, m) {
 
   // to_string functions
 
-  m.def("to_string", py::overload_cast<int>(&to_string<SIMDSTRING_ALIGNMENT>));
-  m.def("to_string", py::overload_cast<long>(&to_string<SIMDSTRING_ALIGNMENT>));
+  m.def("to_string", py::overload_cast<int>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
+  m.def("to_string", py::overload_cast<long>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
   m.def("to_string",
-        py::overload_cast<long long>(&to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<long long>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
   m.def("to_string",
-        py::overload_cast<unsigned int>(&to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<unsigned int>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
   m.def("to_string",
-        py::overload_cast<unsigned long>(&to_string<SIMDSTRING_ALIGNMENT>));
-  m.def("to_string", py::overload_cast<unsigned long long>(
-                         &to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<unsigned long>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
   m.def("to_string",
-        py::overload_cast<float>(&to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<unsigned long long>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
+  m.def("to_string", py::overload_cast<float>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
   m.def("to_string",
-        py::overload_cast<double>(&to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<double>(&to_string<SIMDSTRING_ALIGNMENT>), "value"_a);
   m.def("to_string",
-        py::overload_cast<long double>(&to_string<SIMDSTRING_ALIGNMENT>));
+        py::overload_cast<long double>(&to_string<SIMDSTRING_ALIGNMENT>),
+        "value"_a);
 }
 
 auto main() -> int { return 0; }
